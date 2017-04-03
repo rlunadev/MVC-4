ab51af5ed71caa5457a1f109adb487b521579d48
Ronald <Ronald@DESKTOP-DJ1H9LD>
2017-04-03 11:22:02 -0400

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AjaxTest(string nombre, string apellido)
+        {
+            var fullName = nombre+apellido;
+            
+            
+            return Json(fullName);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/AjaxTestModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class AjaxTestModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+    }
+
+}
